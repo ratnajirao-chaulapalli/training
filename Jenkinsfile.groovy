4ceb41584286848861a7feec9a54929f3b0f92a4
ratnajirao-chaulapalli <42180490+ratnajirao-chaulapalli@users.noreply.github.com>
2021-06-15 23:49:19 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,20 @@
-node("master")
+nodes
+{
+  node("jenkins")
 {
 stage('prepare-gcp-pipeline')
 {
-/*sh 'echo "HELLO"'
+sh 'echo "HELLO"'
 sh 'gcloud init'
-*/
+//bat "cmd /c dir"
+}
+}
+node("master")
+{
+stage('prepare-gcp-pipeline1')
+{
 bat "cmd /c dir"
+}
+
 }
 }
