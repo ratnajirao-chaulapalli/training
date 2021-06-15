b80c0017c7e459c5e8c70d48e3a43b50cd8b055c
ratnajirao-chaulapalli <42180490+ratnajirao-chaulapalli@users.noreply.github.com>
2021-06-15 20:13:47 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,1 +1,9 @@
+ properties([[$class: 'GithubProjectProperty', displayName: '', projectUrlStr: 'https://github.com/ratnajirao-chaulapalli/training/']])properties([[$class: 'GithubProjectProperty', displayName: '', projectUrlStr: 'https://github.com/ratnajirao-chaulapalli/training/']])
 
+node("Jenkins")
+{
+stage('prepare-gcp-pipeline')
+{
+sh 'echo "HELLO"'
+}
+}
